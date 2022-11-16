1e2e244e69dd76dce93843c6ad46f609f55b0002
AliciaPérez <44456885+Aliciap26@users.noreply.github.com>
2022-11-15 17:40:45 -0800

    Add files via upload

---
+++ Sensores/KY-039/ky-039.py
@@ -0,0 +1,11 @@
+from machine import ADC
+from utime import sleep
+
+sensor = ADC(26)
+
+conversion_factor = 3 / (635)
+
+while True:
+        lectura = sensor.read_u16() * conversion_factor
+	print(lectura)
+        sleep(0.09)
